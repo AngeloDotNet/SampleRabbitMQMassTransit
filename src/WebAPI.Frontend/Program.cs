8b1697194a741468670ebc0c76f62e63772f0372
Angelo Pirola <angelo9871@gmail.com>
2023-04-11 20:05:27 +0200

    WIP: Progetto WebAPI.Frontend

---
+++ src/WebAPI.Frontend/Program.cs
@@ -0,0 +1,19 @@
+namespace WebAPI.Frontend;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+        Startup startup = new(builder.Configuration);
+
+        startup.ConfigureServices(builder.Services);
+
+        WebApplication app = builder.Build();
+
+        startup.Configure(app);
+
+        app.Run();
+    }
+}
